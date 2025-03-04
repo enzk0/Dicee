c4d043b88e64f60b61d6407f8bfeb1016422b7ad
enzk0 <renzoroel24@gmail.com>
2025-03-04 22:36:17 +0800

    Added function to change images

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -6,4 +6,48 @@ function randomNumber2() {
     var num2 = Math.floor(Math.random() * 6) + 1;
 }
 
+function randomDice1(number) {
+    switch(number) {
+        case 1:
+          document.querySelector(".img1").src = "./images/dice1.png";
+          break;
+        case 2:
+            document.querySelector(".img1").src = "./images/dice2.png";
+            break;
+        case 3:
+          document.querySelector(".img1").src = "./images/dice3.png";
+          break;
+        case 4:
+            document.querySelector(".img1").src = "./images/dice4.png";
+            break;
+        case 5:
+          document.querySelector(".img1").src = "./images/dice5.png";
+          break;
+        case 6:
+          document.querySelector(".img1").src = "./images/dice6.png";
+          break;
+      }
+}
 
+function randomDice2(number) {
+    switch(number) {
+        case 1:
+          document.querySelector(".img2").src = "./images/dice1.png";
+          break;
+        case 2:
+            document.querySelector(".img2").src = "./images/dice2.png";
+            break;
+        case 3:
+          document.querySelector(".img2").src = "./images/dice3.png";
+          break;
+        case 4:
+            document.querySelector(".img2").src = "./images/dice4.png";
+            break;
+        case 5:
+          document.querySelector(".img2").src = "./images/dice5.png";
+          break;
+        case 6:
+          document.querySelector(".img2").src = "./images/dice6.png";
+          break;
+      }
+}
